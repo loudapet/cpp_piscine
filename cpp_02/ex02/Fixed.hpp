9cdce5cfdf052d515fc9624d8a653dcb478aa2f1
Petr Louda <plouda@student.42prague.com>
2024-03-12 16:47:46 +0100

    cpp02 ex02 (overloading operators) hopefully done

---
+++ cpp_02/ex02/Fixed.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plouda <plouda@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 10:06:17 by plouda            #+#    #+#             */
+/*   Updated: 2024/03/12 16:42:35 by plouda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+	private:
+		int					_value;
+		static const int	_FRACTIONALBITS;
+	
+	public:
+
+		Fixed();
+		Fixed(const int integer);
+		Fixed(const float floatingNb);
+		Fixed(const Fixed& oldObj);
+		Fixed&	operator=(const Fixed& oldObj);
+		~Fixed();
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+		bool	operator<(const Fixed& right) const;
+		bool	operator>(const Fixed& right) const;
+		bool	operator>=(const Fixed& right) const;
+		bool	operator<=(const Fixed& right) const;
+		bool	operator==(const Fixed& right) const;
+		bool	operator!=(const Fixed& right) const;
+		Fixed	operator+(const Fixed& right) const;
+		Fixed	operator-(const Fixed& right) const;
+		Fixed	operator*(const Fixed& right) const;
+		Fixed	operator/(const Fixed& right) const;
+		Fixed&	operator++();
+		Fixed	operator++(int);
+		Fixed&	operator--();
+		Fixed	operator--(int);
+		static Fixed&	min(Fixed& first, Fixed& second);
+		static Fixed&	max(Fixed& first, Fixed& second);
+		static const Fixed&	min(const Fixed& first, const Fixed& second);
+		static const Fixed&	max(const Fixed& first, const Fixed& second);
+		
+};
+
+std::ostream&	operator<<(std::ostream &outputStream, const Fixed &fixedNb);
+
+#endif
---
+++ cpp_02/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plouda <plouda@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 10:20:33 by plouda            #+#    #+#             */
+/*   Updated: 2024/03/12 16:43:04 by plouda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void )
+{
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}
