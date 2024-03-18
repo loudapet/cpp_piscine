3691ca9dd4a5a65d2790844bf0b31c9feae72532
Petr Louda <plouda@student.42prague.com>
2024-03-18 14:04:00 +0100

    Updated tests for BSP

--- cpp_02/ex03/main.cpp
+++ cpp_02/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plouda <plouda@student.42.fr>              +#+  +:+       +#+        */
+/*   By: plouda <plouda@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 10:20:33 by plouda            #+#    #+#             */
-/*   Updated: 2024/03/13 14:12:30 by plouda           ###   ########.fr       */
+/*   Updated: 2024/03/18 14:02:10 by plouda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,16 @@ int main( void )
 	const Point	a(0.,0.);
 	const Point	b(0.,10.);
 	const Point c(10.,0.);
-	const Point testedPoint(5,5);
+	const Point P1(0,0);
+	const Point P2(5,5);
+	const Point P3(9,10);
+	const Point P4(-5,5);
+	const Point P5(1,1);
 
-	std::cout << (bsp(a, b, c, testedPoint) ? "in" : "out") << std::endl;
+	std::cout << (bsp(a, b, c, P1) ? "in" : "out") << std::endl;
+	std::cout << (bsp(a, b, c, P2) ? "in" : "out") << std::endl;
+	std::cout << (bsp(a, b, c, P3) ? "in" : "out") << std::endl;
+	std::cout << (bsp(a, b, c, P4) ? "in" : "out") << std::endl;
+	std::cout << (bsp(a, b, c, P5) ? "in" : "out") << std::endl;
 	return 0;
 }
