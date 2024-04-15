2e48fd21a7f24c1f95a1e7b7b21080498eddf0eb
Petr Louda <plouda@student.42prague.com>
2024-04-15 09:06:57 +0200

    Edited testing main

--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plouda <plouda@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 10:16:20 by plouda            #+#    #+#             */
-/*   Updated: 2024/04/12 12:39:33 by plouda           ###   ########.fr       */
+/*   Updated: 2024/04/15 09:05:56 by plouda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,16 @@ int	main(void)
 {
 	std::vector<int> vec;
 	srand(time(0));
+	for (int i = 0; i < 10; ++i)
+		vec.push_back(rand() % 50);
 
-	// Adding random numbers to the vector
-	for (int i = 0; i < 10; ++i) {
-		vec.push_back(rand() % 100); // Random numbers between 0 and 99
-	}
-
-	// Print the vector using an iterator
-	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) {
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
 		std::cout << *it << " ";
-	}
 	std::cout << std::endl;
 	
-	//test easyfind part
 	try
 	{
-		{::easyfind(vec, 42);}
+		easyfind(vec, 10);
 	}
 	catch(const std::exception& e)
 	{
@@ -40,7 +34,7 @@ int	main(void)
 
 	try
 	{
-		{::easyfind(vec, 1);}
+		easyfind(vec, 21);
 	}
 	catch(const std::exception& e)
 	{
@@ -49,12 +43,12 @@ int	main(void)
 
 	try
 	{
-		{::easyfind(vec, 96);}
+		easyfind(vec, 42);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
 	
-return 0;
+	return (0);
 }
